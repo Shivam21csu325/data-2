85ad78a22db6ce85cdb09adbd4d7328f6037a629
shivam 21csu325 <shivam21csu325@ncuindia.edu>
2024-01-01 17:35:56 +0530

    finding error

--- hi.java
+++ hi.java
@@ -1,4 +1,13 @@
 class hi {
     public static void main(String[] args) {
-    }
+        int x = 10;
+        System.out.println("x is " + x); // prints: x is 10
+        if (x > 5) {
+            System.out.println("x is greater than 5"); // this line will be executed because the condition in the 'if' statement evalu
+            System.out.println("x is greater than 5"); // this line will be executed because the condition is true
+            System.out.println("x is greater than 5"); // prints: x is greater than 5
+            } else {
+                System.out.println("x is not greater than 5"); // does not print anything
+                }
+        }
 }
