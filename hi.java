f0a17c150c3efc3598ce60cc36840e1691031cc2
shivam 21csu325 <shivam21csu325@ncuindia.edu>
2024-01-01 17:00:45 +0530

    my name

--- hi.java
+++ hi.java
@@ -2,7 +2,6 @@ class hi {
     public static void main(String[] args) {
         System.out.println("Hi, World!");
         System.out.println("Hi, ");
-        System.out.println("world!");
-
+        System.out.println("world! it shivam bhardwaj");
     }
 }
