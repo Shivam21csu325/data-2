b4121bd05b33375aaa08d61c78e539a6df8686d2
shivam 21csu325 <shivam21csu325@ncuindia.edu>
2024-01-01 15:39:56 +0530

    hi world

--- hi.java
+++ hi.java
@@ -1,5 +1,7 @@
 class hi {
     public static void main(String[] args) {
-        System.out.println("Hi, World!"); 
+        System.out.println("Hi, World!");
+        System.out.println("Hi");
+        System.out.println("world");
     }
 }
